f37428b1f41e80cccb32e6e992791933eac1dbae
Bagus Rinu Pangayom <bagusrinup@gmail.com>
2023-12-30 21:23:40 +0700

    refacor: add content for careers component 1. header 2. main content

--- src/components/careersComp.jsx
+++ src/components/careersComp.jsx
@@ -3,13 +3,41 @@ import { Container } from 'react-bootstrap';
 const CareersComp = () => {
     return (
         <>
-            <div>
-                <section>
-                    <Container>
-                        <h1>hahaha</h1>
-                    </Container>
-                </section>
+            <div className="bgCareers">
+                <Container>
+                    <div className="careersHeader text-center">
+                        <h1>CAREERS</h1>
+                    </div>
+                </Container>
             </div>
+
+            <section>
+                <Container>
+                    <div className="openPositions">
+                        <div className="openPositionsHeading mb-4">
+                            <h1>Open Positions</h1>
+                        </div>
+
+                        <div className="mb-4">
+                            <h2>01</h2>
+                            <h2>Health and Safety Coordinator - Hamburg, Germany</h2>
+                            <p>FULL-TIME</p>
+                        </div>
+
+                        <div className="mb-4">
+                            <h2>02</h2>
+                            <h2>Construction Safety Manager - Hamburg, Germany</h2>
+                            <p>FULL-TIME</p>
+                        </div>
+
+                        <div>
+                            <h2>03</h2>
+                            <h2>Construction Engineer - Hamburg, Germany</h2>
+                            <p>FULL-TIME</p>
+                        </div>
+                    </div>
+                </Container>
+            </section>
         </>
     );
 };
