55bd7af29feaac3dd466a086db4b3f31eef13e77
Bagus Rinu Pangayom <bagusrinup@gmail.com>
2023-12-30 21:49:16 +0700

    refactor: create content for contacts component 1. header 2. main content

--- src/components/contactsComp.jsx
+++ src/components/contactsComp.jsx
@@ -1,15 +1,42 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 
 const ContactComp = () => {
     return (
         <>
-            <div>
-                <section>
-                    <Container>
+            <div className="bgContacts">
+                <Container>
+                    <div className="contactsHeader text-center">
                         <h1>CONTACTS</h1>
-                    </Container>
-                </section>
+                    </div>
+                </Container>
             </div>
+
+            <section className="contentContacts">
+                <Container>
+                    <div className="mb-4">
+                        <h1>We would love to hear from you!</h1>
+                    </div>
+
+                    <Row className="pt-4">
+                        <Col sm={6} className="mb-4">
+                            <div>
+                                <h2>Visit us:</h2>
+                                <div>Raden Intan Street 6</div>
+                                <div>Bandar Lampung, Indonesia</div>
+                            </div>
+                        </Col>
+
+                        <Col sm={6}>
+                            <div>
+                                <h2>Get in touch:</h2>
+                                <div>Call us: +0898765432101</div>
+                                <div>Write to us: info@kulimas.com</div>
+                                <div>Career inquiries: career@kulimas.com</div>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
         </>
     );
 };
